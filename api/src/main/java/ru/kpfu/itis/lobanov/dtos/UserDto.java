85f953d42eb42e490b321ddfaa173ce5a81971bb
Nikita Lobanov <lobanov.nikita2018@yandex.ru>
2024-04-30 17:18:32 +0300

    implement microservices gateway and registry service

--- api/src/main/java/ru/kpfu/itis/lobanov/dtos/UserDto.java
+++ api/src/main/java/ru/kpfu/itis/lobanov/dtos/UserDto.java
@@ -5,12 +5,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-import java.util.Collections;
 
 @Data
 @Builder
